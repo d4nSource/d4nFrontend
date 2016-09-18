e2979e12c2b94fcb950701e2be0801a2223c2f0f
Dorian Szekely <dorian.szekely@gmail.com>
2016-09-18 08:49:57 +0200

    simplify data driven forms

--- src/app/user/signup.component.ts
+++ src/app/user/signup.component.ts
@@ -12,11 +12,32 @@ export class SignupComponent implements OnInit {
     constructor(private _formBuilder: FormBuilder) { }
 
     ngOnInit() {
-       this.signupForm = new FormGroup({
-           'username': new FormControl('', Validators.required),
-           'email': new FormControl('', Validators.required),
-           'password': new FormControl('', Validators.required)
+//       this.signupForm = new FormGroup({
+//           'username': new FormControl('', Validators.required),
+//           'email': new FormControl('', Validators.required),
+//           'password': new FormControl('', Validators.required)
+ //       });
+        this.signupForm = this._formBuilder.group({
+            'username': ['', [Validators.required, this.customValidator]],
+            'email': ['', [
+                    Validators.required,
+                    Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$')
+                    ]],
+            'password': ['', Validators.required]
         });
+     }
+
+     customValidator(control: FormControl): {[s: string]: boolean} {
+         // set the condition for an invalid case!!
+         if (control.value === 'Administrator') {
+             return {invalid: true};
+         }
+         // otherwise its valid
+         return null;
+     }
+
+     asyncValidator(control: FormControl): Promise<any> | any {
+
      }
 
     onSubmit() {
